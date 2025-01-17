8e748a17e652758bccdf7969ed8fd6c96facff7c
Admin <naruto917147@gmail.com>
2025-01-18 00:30:56 +0700

    phase4
    phase3

--- Assets/Scripts/EmotionManager.cs
+++ Assets/Scripts/EmotionManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EmotionManager : MonoBehaviour
 {
@@ -7,13 +8,31 @@ public class EmotionManager : MonoBehaviour
     public float angry;
     public float scared;
 
+    public float energy;
+    public float maxEnergyValue = 20f;
+
     public float maxEmotionValue = 10f;
+
+    public Button happyButton;
+    public Button sadButton;
+    public Button angryButton;
+    public Button scaredButton;
+    public Button energyButton; 
+
+    void Start()
+    {
+        happyButton.onClick.AddListener(() => AdjustEmotion("happy", 1f));
+        sadButton.onClick.AddListener(() => AdjustEmotion("sad", 1f));
+        angryButton.onClick.AddListener(() => AdjustEmotion("angry", 1f));
+        scaredButton.onClick.AddListener(() => AdjustEmotion("scared", 1f));
+        energyButton.onClick.AddListener(() => AdjustEnergy(1f)); 
+    }
 
     void Update()
     {
         CheckDominantEmotion();
 
-        Debug.Log($"Happy: {happy}, Sad: {sad}, Angry: {angry}, Scared: {scared}");
+        Debug.Log($"Happy: {happy}, Sad: {sad}, Angry: {angry}, Scared: {scared}, Energy: {energy}");
     }
 
     void CheckDominantEmotion()
@@ -37,6 +56,12 @@ public class EmotionManager : MonoBehaviour
         {
             scared = maxEmotionValue;
             Debug.Log("Scared State");
+        }
+
+        if (energy >= maxEnergyValue)
+        {
+            energy = maxEnergyValue;
+            Debug.Log("Energy is full!");
         }
     }
 
@@ -71,5 +96,14 @@ public class EmotionManager : MonoBehaviour
         sad = Mathf.Max(0, sad);
         angry = Mathf.Max(0, angry);
         scared = Mathf.Max(0, scared);
+    }
+
+    public void AdjustEnergy(float amount)
+    {
+        energy = Mathf.Min(maxEnergyValue, energy + amount);
+        if (energy >= maxEnergyValue)
+        {
+            Debug.Log("Energy is full!");
+        }
     }
 }

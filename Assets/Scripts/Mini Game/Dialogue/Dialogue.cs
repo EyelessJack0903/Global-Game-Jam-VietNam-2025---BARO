1c2c028526b2839965e1bd12a1a1431c256605ea
EyelessJack0903 <eyelessjack0903@gmail.com>
2025-01-18 19:04:59 +0700

    update hội thoại

---
+++ Assets/Scripts/Mini Game/Dialogue/Dialogue.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Dialogue", menuName = "Dialogue/DialogueCharacter", order = 1)]
+public class Dialogue : ScriptableObject
+{
+    [TextArea(3, 10)]
+    public string dialogueText;
+
+    public bool isPositive; // tích cực (true), tiêu cực (false)
+    public string speakerName;
+    public List<Dialogue> nextDialogues;
+    public bool isChoice;
+    public float delayTime = 2f;
+}

1bf488d86baeb3b13c1d0fe55164120ee9dfb6f1
EyelessJack0903 <eyelessjack0903@gmail.com>
2025-01-19 06:10:55 +0700

    update menu

---
+++ Assets/Scripts/GameOverMaanager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverMaanager : MonoBehaviour
+{
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MENU");
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false; 
+#else
+        Application.Quit(); // Thoát ứng dụng trong phiên bản build
+#endif
+    }
+}
